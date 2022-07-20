0aa8f5929bdb17753930d838081dc8913375ee54
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:09:58 +0530

    Add files via upload

---
+++ Automobiles.java
@@ -0,0 +1,34 @@
+package Interface;
+
+public class Automobiles implements cars,Bikes {
+public void Audi() {
+	System.out.println("Audi");
+}
+public void Ferrai() {
+	System.out.println("Ferrai");
+}
+public void BMW() {
+	System.out.println("BMW");
+}
+public void Skoda() {
+	System.out.println("Skoda");
+}
+public void HardleyDavidson() {
+	System.out.println("Hardley Davidson");
+}
+public void KTM() {
+	System.out.println("KTM");
+}
+public void Ducati() {
+	System.out.println("Ducati");
+}
+public void Gixer() {
+	System.out.println("Gixer");
+}
+public void axle() {
+	System.out.println("Axle");
+}
+public void engine() {
+	System.out.println("Engine");
+}
+}
