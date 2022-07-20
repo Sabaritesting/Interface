62fe34eee433b23dc9542d8d7f8c77b02394cc08
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:10:13 +0530

    Add files via upload

---
+++ Testautomobile.java
@@ -0,0 +1,25 @@
+package Interface;
+
+public class Testautomobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Automobiles a=new Automobiles();
+a.Audi();
+a.BMW();
+a.Ferrai();
+a.Skoda();
+a.HardleyDavidson();
+a.KTM();
+a.Ducati();
+a.Gixer();
+a.axle();
+a.engine();
+cars c=new Automobiles();
+c.Audi();
+c.BMW();
+c.Ferrai();
+c.Skoda();
+	}
+
+}
